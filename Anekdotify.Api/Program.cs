f44b657772238e4efab8c1edf97b1cd0d5e5758e
RustamHolov <roshfor22@gmail.com>
2025-06-12 11:15:31 +0200

    refactor: - Implement authentication services in Program.cs. - Update CommentCard.razor to improve comment display, add reply functionality, and enhance delete confirmation modal. - Modify Login.razor layout and change login button style for better UI consistency.

--- Anekdotify.Api/Program.cs
+++ Anekdotify.Api/Program.cs
@@ -120,7 +120,6 @@ builder.Services.AddAuthentication(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
